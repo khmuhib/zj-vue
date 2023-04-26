18fbc80e9349f103a72a21e6968855976eea6560
khmuhib <khmuhib2013@gmail.com>
2023-04-26 21:55:59 +0600

    image added

--- src/views/CoutureView.vue
+++ src/views/CoutureView.vue
@@ -2,7 +2,14 @@
     <div class="pb-5">
         <div class="container m-auto">
             <div class="flex flex-col gap-5">
-                <img v-for="list in imgList" :key="list.id" class="px-5" :src="`@/assets/images/${list.img}`" alt="couture">
+                <img class="px-5" src="./../assets/images/couture01.jpg" alt="">
+                <img class="px-5" src="./../assets/images/couture02.jpg" alt="">
+                <img class="px-5" src="./../assets/images/couture03.jpg" alt="">
+                <img class="px-5" src="./../assets/images/couture04.jpg" alt="">
+                <img class="px-5" src="./../assets/images/couture05.jpg" alt="">
+                <img class="px-5" src="./../assets/images/couture06.jpg" alt="">
+                <img class="px-5" src="./../assets/images/couture07.jpg" alt="">
+                <img class="px-5" src="./../assets/images/couture08.jpg" alt="">
             </div>
         </div>
     </div>
