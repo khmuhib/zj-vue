42d4f37c4c74e52397e8c3d3c20ac5736dcca8e0
khmuhib <khmuhib2013@gmail.com>
2023-04-29 02:00:27 +0600

    ...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@ export default {
 
 <template>
   <div class="pb-5">
-    <div class="container m-auto px-5 min-h-[60vh]">
+    <div class="container m-auto px-5 min-h-[55vh]">
       <div class="w-[80%] m-auto">
         <vue-load-image>
           <template v-slot:image>
