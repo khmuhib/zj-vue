f591b5f0c78b29e818fc07f0b74d32812477ed81
khmuhib <khmuhib2013@gmail.com>
2023-05-05 23:13:04 +0600

    site optimize

--- src/router/index.js
+++ src/router/index.js
@@ -31,11 +31,6 @@ const router = createRouter({
       component: () => import('../views/AppointmentView.vue')
     },
     {
-      path: '/test',
-      name: 'test',
-      component: () => import('../views/Test.vue')
-    },
-    {
       path: '/legal',
       name: 'legal',
       component: () => import('../views/LegalView.vue')
